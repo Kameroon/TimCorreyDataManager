c8e39750f66d19d1b3d655fca24a3840e828f185
DAVID MIKE <aidipifr@hotmail.com>
2019-08-03 23:37:44 +0200

    Prémier commit

---
+++ MMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using MMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private const string VIEWMODEL = "ViewModel";
+
+        // Pour gèrer l'injection de dépendences
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            //// Pour appliquer l'injection de dépendence
+            //_container.RegisterPerRequest<>Interface, Class);
+
+            /*
+             * Avec la reflection, Dans l'assembly on recherche toutes les classes qui se terminent par ViewModel 
+             * Suivant la demande enregistre dans mon container
+             * */
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith(VIEWMODEL))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);  
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ MMDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDesktopUI.ViewModels
+{
+    public class ShellViewModel
+    {
+        #region --  --
+       
+        #endregion
+
+        #region --  --
+        public ShellViewModel()
+        {
+
+        }
+        #endregion
+
+        #region --  --
+
+        #endregion
+    }
+}
